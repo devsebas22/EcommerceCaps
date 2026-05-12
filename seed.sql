405c9b5e407117e3ec3e6d0097ff901a3f19235c
devsebas22 <s.mogollon@utp.edu.co>
2026-05-11 20:38:09 -0500

    feat: agregar imagenes de productos al seed

--- seed.sql
+++ seed.sql
@@ -11,3 +11,10 @@ INSERT INTO productos (nombre, descripcion, precio, marca, stock, imagen_url, ca
 ('Reloj Casio Vintage', 'Reloj clásico digital', 120000, 'Casio', 8, null, 3),
 ('Locion Gucci', '100% original no fake', 1000000, 'Gucci', 10, null, 2),
 ('Gorra Puma BMW', 'Gorra deportiva Puma edicion BMW', 55000, 'Puma', 4, null, 1);
+
+-- Imagenes de productos
+INSERT INTO producto_imagenes (producto_id, url, es_principal) VALUES
+(1, 'https://i.ibb.co/S7R8Xkdq/Gorra-nike.jpg', true),
+(2, 'https://i.ibb.co/PZ8X9xn1/armanicode.jpg', true),
+(3, 'https://i.ibb.co/WWWcsWGf/casio-vintage.jpg', true),
+(4, 'https://i.ibb.co/h1Tr25MD/armanicode.jpg', true);
